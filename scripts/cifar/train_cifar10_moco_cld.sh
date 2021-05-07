28628d30cabd8b5589546fb2362dcfe468ef5264
Xudong (Frank) Wang <58996472+frank-xwang@users.noreply.github.com>
2021-05-06 20:24:52 -0700

    Update train_cifar10_moco_cld.sh

--- scripts/cifar/train_cifar10_moco_cld.sh
+++ scripts/cifar/train_cifar10_moco_cld.sh
@@ -30,4 +30,5 @@ python -m torch.distributed.launch --master_port 1233${GPU_ID} --nproc_per_node=
     --normlinear \
     --aug-plus \
     --erasing \
+    --clusters ${clusters} \
     --save-dir "checkpoint/${dataset}/MoCo+CLD/resnet18/lr0.03-bs${bs}-cldT${cld_t}-nceT${nce_t}-clusters${clusters}-lambda${lambda}-${scheduler}-weightDecay${weightdecay}-fp16-add_erasing-AugPlus-kMeans-ncek${nce_k}-bslr${bs_lr}-normlinear" \
